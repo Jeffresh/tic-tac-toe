f051cbf8eaa87c0167c7652538390f90b494874d
Jeff <jeff.uca@gmail.com>
2020-07-22 16:04:48 +0200

    :sparkles: final state of tic tac toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,30 +1,19 @@
 class TicTacToe:
 
     def __init__(self):
-        self.rows = []
-
-    def empty_table(self, ):
-        print('-' * 9)
-        print('| _ _ _ |')
-        print('| _ _ _ |')
-        print('| _ _ _ |')
-        print('-' * 9)
+        self.rows = [['_', '_', '_'] for _ in range(3)]
 
     def who_won(self, player):
-        return (self.rows[0][0] == player and self.rows[0][1] == player and self.rows[0][2] == player \
-                or self.rows[1][0] == player and self.rows[1][1] == player and self.rows[1][2] == player \
-                or self.rows[2][0] == player and self.rows[2][1] == player and self.rows[2][2] == player \
-                or self.rows[0][0] == player and self.rows[1][0] == player and self.rows[2][0] == player \
-                or self.rows[0][1] == player and self.rows[1][1] == player and self.rows[2][1] == player \
-                or self.rows[0][2] == player and self.rows[1][2] == player and self.rows[2][2] == player \
-                or self.rows[0][0] == player and self.rows[1][1] == player and self.rows[2][2] == player \
-                or self.rows[0][2] == player and self.rows[1][1] == player and self.rows[2][0] == player)
+        return (any(all([True if self.rows[i][j] == player else False for j in range(3)]) for i in range(3)) \
+                or any(all([True if self.rows[j][i] == player else False for j in range(3)]) for i in range(3)) \
+                or all(True if self.rows[i][i] == player else False for i in range(3)) \
+                or all(True if self.rows[2 - i][i] == player else False for i in range(3)))
 
     def initialize_state(self):
         table = list(input('Enter cells:'))
         self.rows = [[table[i] for i in range(n, n + 3)] for n in (0, 3, 6)]
 
-    def user_move(self):
+    def user_move(self, player):
 
         moved = False
         while not moved:
@@ -41,39 +30,48 @@ class TicTacToe:
                     x, y = 3 - y, x - 1
 
                     if self.rows[x][y] in ['_', ' ']:
-                        self.rows[x][y] = 'X'
+                        self.rows[x][y] = player
                         moved = True
                     else:
                         print('This cell is occupied! Choose another one!')
 
-    def state(self):
+    def print_state(self):
+        print('---------')
+        for row in self.rows:
+            print('|', *row, '|')
+        print('---------')
 
+    def game_state(self):
         x_number = sum(l.count('X') for l in self.rows)
         o_number = sum(l.count('O') for l in self.rows)
 
-        if x_number + o_number == 0:
-            self.empty_table()
-        else:
-            print('---------')
-            for row in self.rows:
-                print('|', *row, '|')
-            print('---------')
+        game_ended = False
 
-        # if abs(x_number - o_number) > 1 or self.who_won('X') and self.who_won('O'):
-        #     print('Impossible')
-        # elif self.who_won('X'):
-        #     print('X wins')
-        # elif self.who_won('O'):
-        #     print('O wins')
-        # elif x_number + o_number != 9:
-        #     print('Game not finished')
-        # else:
-        #     print('Draw')
+        if self.who_won('X'):
+            print('X wins')
+            game_ended = True
+        elif self.who_won('O'):
+            print('O wins')
+            game_ended = True
+        elif x_number + o_number != 9:
+            print('Game not finished')
+        else:
+            print('Draw')
+            game_ended = True
+
+        return game_ended
+
+    def new_game(self):
+        players = ['X', 'O']
+        time = 0
+        self.print_state()
+        while not self.game_state():
+            self.user_move(players[time % 2])
+            self.print_state()
+            time += 1
+        print('This game ends in {} rounds!'.format(time))
 
 
 if __name__ == '__main__':
     my_game = TicTacToe()
-    my_game.initialize_state()
-    my_game.state()
-    my_game.user_move()
-    my_game.state()
+    my_game.new_game()
